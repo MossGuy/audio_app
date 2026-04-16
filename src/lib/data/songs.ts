b431b68d649736f4ec094aac5958d1231be78627
MossGuy <milanpallas@hotmail.nl>
2026-04-16 16:22:19 +0200

    added new song

--- src/lib/data/songs.ts
+++ src/lib/data/songs.ts
@@ -13,5 +13,10 @@ export const songs = [
         id: 3,
         title: "Sootopolis City (Pokémon Ruby_Sapphire) Jazz Cover",
         url: "https://pub-9c10e61d538c4d1285bd2bc9e7ed497d.r2.dev/Sootopolis%20City%20(Pok%C3%A9mon%20Ruby_Sapphire)%20Jazz%20Cover%20-%20The%20Consouls%20(1)%20(mp3cut.net).mp3"
+    },
+    {
+        id: 4,
+        title: "Pokemon Mystery Dungeon 3 Hours of Relaxing Music",
+        url: "https://pub-9c10e61d538c4d1285bd2bc9e7ed497d.r2.dev/Pokemon%20Mystery%20Dungeon%203%20Hours%20of%20Relaxing%20Music.mp3"
     }
 ];
